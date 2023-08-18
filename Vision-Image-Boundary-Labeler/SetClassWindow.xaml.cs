513bbe8cd1c016b3311fd30dfa5dfd44edf97c64
Awesome <awesome_devnet@outlook.com>
2023-08-18 13:16:02 +0900

    feat

--- Vision-Image-Boundary-Labeler/SetClassWindow.xaml.cs
+++ Vision-Image-Boundary-Labeler/SetClassWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System.IO;
+﻿using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using Vision_Image_Boundary_Labeler.Domain;
 using Vision_Image_Boundary_Labeler.Json;
+using Microsoft.VisualBasic;
 
 namespace Vision_Image_Boundary_Labeler {
     /// <summary>
@@ -9,6 +11,8 @@ namespace Vision_Image_Boundary_Labeler {
     /// </summary>
     public partial class SetClassWindow : Window {
         private LabelClassManager labelClassManager = new();
+
+        public string DirectoryPath { get; set; }
         private static int IndexNumber = 0;
         public SetClassWindow() {
             InitializeComponent();
@@ -37,6 +41,17 @@ namespace Vision_Image_Boundary_Labeler {
 
         private void btnSave_Click(object sender, RoutedEventArgs e) {
             labelClassManager.Save();
+        }
+
+        private void ClassListView_OnMouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            // 선택된 항목을 BoundaryClass 타입으로 가져옴
+            var selectedItem = ClassListView.SelectedItem as BoundaryClass;
+            if(selectedItem != null) {
+                string reply = Interaction.InputBox($"[{selectedItem.ClassIndex}]번 클래스 항목의 이름을 변경합니다. \n변경할 이름을 입력하세요.\n\n현재 설정값 : {selectedItem.ClassName}", "Title", $"{selectedItem.ClassName}");
+                selectedItem.ClassName = reply;
+
+                RefreshListView();
+            }
         }
     }
 }
